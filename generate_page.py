dbffa778b5799ad4306eef5efdea2577a1d6fdf1
Anthony Tan <tony.face2ai@gmail.com>
2024-05-09 11:27:06 +0800

    code highlight

--- generate_page.py
+++ generate_page.py
@@ -5,14 +5,25 @@ sections = [
     {
         'id': 'section-0',
         'title': 'LLM.int() on GPT-NeoX',
-        'description': '## test makdown \nThis implements a utility function to transform an nn.Linear layer\n to LLM.int8() linear layer.',
-        'code': 'def transform_layer(layer):\npass  # Example code snippet'
+        'description': '## test makdown '+r'$\\delta$' +'\nThis implements a utility function to transform an nn.Linear layer\n to <code>LLM.int8()</code> linear layer.',
+        'lang':'python',
+        'code': [{'line_num': '1',
+                  'content': 'def transform_layer(layer):'},
+                 {'line_num': '2',
+                  'content': '    pass  # Example code snippet'}
+                 ]
     },
     {
         'id': 'section-1',
         'title': 'Import bitsandbytes',
-        'description': '### test latex \n '+r'$\\frac{1}{1}$'+'\n\nImport the bitsandbytes package to enable specific functions.',
-        'code': 'try:\n    from bitsandbytes import something\nexcept ImportError:\n    print("Install the package")'
+        'description': '### test latex \n ' + r'$\\frac{1}{1}$' + '\n\nImport the bitsandbytes package to enable specific functions.',
+        'lang':'python',
+        'code':
+            [{'line_num': '4', 'content': 'try:'},
+             {'line_num': '5', 'content': '    from bitsandbytes import something'},
+             {'line_num': '6', 'content': '    print("Package imported successfully")'},
+             {'line_num': '7', 'content': 'except ImportError:'},
+             {'line_num': '8', 'content': '    print("Install the package")'}]
     }
     # Add more sections as needed
 ]
